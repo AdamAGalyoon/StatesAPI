5718b6c9145804f76899424162ca6b9e88f8b668
AdamAGalyoon <144166903+AdamAGalyoon@users.noreply.github.com>
2024-05-09 21:54:01 -0700

    Update populateDB.js

--- populateDB.js
+++ populateDB.js
@@ -1,1 +1,18 @@
+const mongoose = require('mongoose');
+const State = require('./models/State');
+const statesData = require('./states.json');
+require('dotenv').config();
 
+async function insertStatesData() {
+  try {
+    await mongoose.connect(process.env.MONGODB_URI);
+
+    console.log('MongoDB connected');
+
+  } finally {
+    await mongoose.disconnect();
+    console.log('Disconnected from MongoDB');
+  }
+}
+
+insertStatesData();
